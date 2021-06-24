5595c2e6008b05048291af0e1d94c24d492796c2
Gregory Flores <gn8967@wayne.edu>
2021-06-23 21:48:56 -0400

    Building the playGame() method.

--- src/game.java
+++ src/game.java
@@ -1,16 +1,9 @@
 // The game is known as 21 or BlackJack
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class game {
-    // Creating the deck of cards
-    card[] deck = {
-            new card("Spades",2), new card("Spades",3), new card("Spades",4), new card("Spades",5), new card("Spades",6), new card("Spades",7), new card("Spades",8), new card("Spades",9), new card("Spades",10), new card("Spades",11), new card("Spades",12), new card("Spades",13), new card("Spades",14),
-            new card("Clubs",2), new card("Clubs",3), new card("Clubs",4), new card("Clubs",5), new card("Clubs",6), new card("Clubs",7), new card("Clubs",8), new card("Clubs",9), new card("Clubs",10), new card("Clubs",11), new card("Clubs",12), new card("Clubs",13), new card("Clubs",14),
-            new card("Hearts",2), new card("Hearts",3), new card("Hearts",4), new card("Hearts",5), new card("Hearts",6), new card("Hearts",7), new card("Hearts",8), new card("Hearts",9), new card("Hearts",10), new card("Hearts",11), new card("Hearts",12), new card("Hearts",13), new card("Hearts",14),
-            new card("Diamonds",2), new card("Diamonds",3), new card("Diamonds",4), new card("Diamonds",5), new card("Diamonds",6), new card("Diamonds",7), new card("Diamonds",8), new card("Diamonds",9), new card("Diamonds",10), new card("Diamonds",11), new card("Diamonds",12), new card("Diamonds",13), new card("Diamonds",14)
-    };
-
     // Hands
     ArrayList<card> playerHand = new ArrayList<>();
     ArrayList<card> houseHand = new ArrayList<>();
@@ -23,17 +16,53 @@ public class game {
         System.out.println("3. Exit");
     }
 
+    public static void menu2(int currency){
+        System.out.println("Your wallet contains: "+ currency + " chips.");
+        System.out.println("Please select an option:");
+        System.out.println("1. Place an opening bet");
+        System.out.println("2. Exit");
+    }
+
     public static int playGame(int wallet){
+        // arrays and variables
+        Random randomObj = new Random();
+        int playerScore = 0;
+        int houseScore = 0;
+        Scanner read = new Scanner(System.in);
+        String answer = "0";
+        card[] deck = {
+                new card("Spades",2), new card("Spades",3), new card("Spades",4), new card("Spades",5), new card("Spades",6), new card("Spades",7), new card("Spades",8), new card("Spades",9), new card("Spades",10), new card("Spades",11), new card("Spades",12), new card("Spades",13), new card("Spades",14),
+                new card("Clubs",2), new card("Clubs",3), new card("Clubs",4), new card("Clubs",5), new card("Clubs",6), new card("Clubs",7), new card("Clubs",8), new card("Clubs",9), new card("Clubs",10), new card("Clubs",11), new card("Clubs",12), new card("Clubs",13), new card("Clubs",14),
+                new card("Hearts",2), new card("Hearts",3), new card("Hearts",4), new card("Hearts",5), new card("Hearts",6), new card("Hearts",7), new card("Hearts",8), new card("Hearts",9), new card("Hearts",10), new card("Hearts",11), new card("Hearts",12), new card("Hearts",13), new card("Hearts",14),
+                new card("Diamonds",2), new card("Diamonds",3), new card("Diamonds",4), new card("Diamonds",5), new card("Diamonds",6), new card("Diamonds",7), new card("Diamonds",8), new card("Diamonds",9), new card("Diamonds",10), new card("Diamonds",11), new card("Diamonds",12), new card("Diamonds",13), new card("Diamonds",14)
+        };
+        // TODO bet first, then deal cards.
         System.out.println("Playing...");
+        while(true){
+            menu2(wallet);
+            answer = read.nextLine();
+            if (answer.equals("2")){
+                System.out.println("Exiting the table...");
+                return wallet;
+            }
+            else if (answer.equals("1")){
+                System.out.println("Please enter the amount you'd like to bet: ");
+                break;
+            }
+            else {
+                System.out.println("Sorry, your answer is unreadable.");
+            }
+        }
+        System.out.println("Shuffling and dealing...");
+        int deckCount = deck.length - 1;
+
         return wallet;
     }
 
-    // TODO make playable version for console.
+    // Main
     public static void main(String[] args) {
         // Variables
         int wallet = 100;
-        int playerScore = 0;
-        int houseScore = 0;
         //
         Scanner read = new Scanner(System.in);
         String answer = "0";
@@ -48,13 +77,14 @@ public class game {
             switch (answer) {
                 case "1":
                     System.out.println("Joining the table...");
-                    playGame(wallet);
+                    wallet = playGame(wallet);
                     break;
                 case "2":
                     System.out.println("Doing nothing...");
                     break;
                 case "3":
                     System.out.println("Exiting...");
+                    System.exit(0);
                     break;
                 default:
                     System.out.println("Your option was unreadable. Please try again.");
